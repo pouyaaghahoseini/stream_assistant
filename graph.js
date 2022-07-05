72913e0215bbb771b6c613869cc3de2fc831733b
millandavid <71908868+millandavid@users.noreply.github.com>
2022-07-05 18:41:46 -0500

    Added Mood List

--- graph.js
+++ graph.js
@@ -11,54 +11,25 @@ barBtn.addEventListener('click', function () {
 
 pieBtn.addEventListener('click', function () {
   graph = false;
-  console.log('hit');
 });
 
-function graphDisplay() {
-  if (graph) {
-    const ctx = document.getElementById('canvas').getContext('2d');
-    const myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Yes', 'No'],
-        datasets: [
-          {
-            label: 'Chat Polling',
-            data: [70, 30],
-            backgroundColor: 'rgba(108, 74, 126, 0.3)',
-            borderColor: 'rgba(108, 74, 126)',
-            borderWidth: 3,
-          },
-        ],
+const ctx = document.getElementById('canvas').getContext('2d');
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Yes', 'No'],
+    datasets: [
+      {
+        label: 'Chat Polling',
+        data: [70, 30],
+        backgroundColor: 'rgba(108, 74, 126, 0.3)',
+        borderColor: 'rgba(108, 74, 126)',
+        borderWidth: 3,
       },
-      options: {
-        // responesive: false,
-        maintainAspectRatio: false,
-      },
-    });
-  } else if (!graph) {
-    console.log('heyyyy');
-    const ctx = document.getElementById('canvas').getContext('2d');
-    const myChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-        labels: ['Yes', 'No'],
-        datasets: [
-          {
-            label: 'Chat Polling',
-            data: [70, 30],
-            backgroundColor: 'rgba(108, 74, 126, 0.3)',
-            borderColor: 'rgba(108, 74, 126)',
-            borderWidth: 3,
-          },
-        ],
-      },
-      options: {
-        // responesive: false,
-        maintainAspectRatio: false,
-      },
-    });
-  }
-}
-
-graphDisplay();
+    ],
+  },
+  options: {
+    // responesive: false,
+    maintainAspectRatio: false,
+  },
+});
