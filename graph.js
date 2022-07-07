226eefa2a7081477db71116562be8d63f17dce7c
millandavid <71908868+millandavid@users.noreply.github.com>
2022-07-07 16:36:21 -0500

    Button now switches graphs

--- graph.js
+++ graph.js
@@ -1,35 +1,61 @@
 /* first graph*/
 
 var graph = true;
-
 var barBtn = document.getElementById('bar-graph');
 var pieBtn = document.getElementById('pie-graph');
 
+// buttons
+
 barBtn.addEventListener('click', function () {
   graph = true;
+  changeChart(graph);
 });
 
 pieBtn.addEventListener('click', function () {
   graph = false;
+  changeChart(graph);
 });
 
-const ctx = document.getElementById('canvas').getContext('2d');
-const myChart = new Chart(ctx, {
+// Graph data variables
+
+const data = {
+  labels: ['Yes', 'No'],
+  datasets: [
+    {
+      label: 'Chat Polling',
+      data: [70, 30],
+      backgroundColor: 'rgba(108, 74, 126, 0.3)',
+      borderColor: 'rgba(108, 74, 126)',
+      borderWidth: 3,
+    },
+  ],
+};
+
+// config
+const config = {
   type: 'bar',
-  data: {
-    labels: ['Yes', 'No'],
-    datasets: [
-      {
-        label: 'Chat Polling',
-        data: [70, 30],
-        backgroundColor: 'rgba(108, 74, 126, 0.3)',
-        borderColor: 'rgba(108, 74, 126)',
-        borderWidth: 3,
-      },
-    ],
-  },
+  data,
   options: {
-    // responesive: false,
     maintainAspectRatio: false,
   },
-});
+};
+
+// display graph
+let myChart = new Chart(document.getElementById('canvas'), config);
+
+function changeChart(type) {
+  myChart.destroy();
+
+  myChart = new Chart(document.getElementById('canvas'), config);
+
+  if (type) {
+    // myChart = new Chart(document.getElementById('canvas'), config);
+    myChart.config.type = 'bar';
+  } 
+  else if (!type) {
+    // myChart.config.data.datasets.data = [50,50];
+    // myChart = new Chart(document.getElementById('canvas'), config);
+    myChart.config.type = 'pie';
+  }
+  myChart.update();
+}
